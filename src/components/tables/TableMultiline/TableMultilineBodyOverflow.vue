f4f3427148fd97b2427a67822f29d3281bc008b5
ddenis87 <dizdok@yandex.ru>
2020-11-26 06:51:35 +0900

    Ovreflow inwork 26.11

--- src/components/tables/TableMultiline/TableMultilineBodyOverflow.vue
+++ src/components/tables/TableMultiline/TableMultilineBodyOverflow.vue
@@ -3,6 +3,7 @@
     <div class="box-full">
       <slot></slot>
     </div>
+    <div class=" box-overflow_linear-gradient"></div>
   </div>
 </template>
 
@@ -17,13 +18,32 @@ export default {
 
 <style lang="scss" scoped>
 .box-overflow {
+  // position: relative;
   width: inherit;
   height: inherit;
   text-align: inherit;
+  // z-index: 80;
   overflow: hidden;
+  // background-image: linear-gradient(white 50%, grey 100%);
+  
+  &_linear-gradient {
+    // position: sticky;
+    // z-index: 100;
+    // left: 100px;
+    // top: 100px;
+    width: inherit;
+    height: inherit;
+    background-color: red;
+    // background-image: linear-gradient(white 80%, grey 100%);
+    
+  }
   .box-full {
+    // z-index: 80;
     border: 1px solid green;
     text-align: inherit;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;  
   }
 }
 </style>
