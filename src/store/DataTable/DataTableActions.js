c131680e5bcb51b64f09cb65155517cd77512c3b
ddenis87 <dizdok@yandex.ru>
2021-01-08 13:39:54 +0900

    If backend not work

--- src/store/DataTable/DataTableActions.js
+++ src/store/DataTable/DataTableActions.js
@@ -16,8 +16,27 @@ export default {
         state.commit('SET_LIST_OPTION', mutationOption);
         state.dispatch('GET_LIST_DATA', {tableName: option.tableName});
       })
-      .catch(err => {console.log(err)})
-      // .finally(() => state.commit('SET_IS_DATA_LOAD'));
+      .catch(err => {
+        console.log(err);
+
+        // if not work server
+        state.commit('SET_LIST_OPTION', {
+          description: 'Testing table',
+          tableName: 'organisations',
+          data: {
+            id: {value: 'id', label: 'ID', type: 'string'},
+            institution_code: {value: 'institution_code', label: 'Код', type: 'string'},
+            title: {value: 'title', label: 'Наименование', type: 'string'},
+            inn: {value: 'inn', label: 'ИНН', type: 'string'},
+            egrul_status: {value: 'egrul_status', label: 'ЕГРЮЛ статус', type: 'choice', choices: [
+              {display_name: 'Действующий', value: 1},
+              {display_name: 'Спец. указания', value: 2},
+            ]}
+          }
+        });
+        state.dispatch('GET_LIST_DATA', {tableName: 'organisations'});
+      })
+      .finally(() => state.commit('SET_IS_DATA_LOAD'));
   },
   
   GET_LIST_DATA(state, option) {
@@ -33,7 +52,19 @@ export default {
         }
         state.commit('SET_LIST_DATA', mutationOption);
       })
-      .catch(err => {console.log(err)})
+      .catch(err => {
+        console.log(err);
+        state.commit('SET_LIST_DATA', {
+          tableName: 'organisations',
+          clear: true,
+          data: [
+            {id: '1', institution_code: '2', title: 'Test organisations 1', inn: '1111111', egrul_status: 1},
+            {id: '2', institution_code: '3', title: 'Test organisations 2', inn: '1111112', egrul_status: 1},
+            {id: '3', institution_code: '4', title: 'Test organisations 3', inn: '1111113', egrul_status: 1},
+            {id: '4', institution_code: '5', title: 'Test organisations 4', inn: '1111114', egrul_status: 1},
+          ],
+        });
+      })
       .finally(() => state.commit('SET_IS_DATA_LOAD'));
   },
   GET_LIST_DATA_NEXT(state, option) {
